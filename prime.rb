8c20d26609c66b991c4d18de24408aadcd0d2ecd
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-03 16:50:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
+require 'benchmark'
 # Add  code here!
+def prime?(number)
+
+  if number <= 3
+    return number > 1
+  end
+
+  if (2..number - 1).any? { |i| number % i == 0 }
+    return false
+  end
+
+  return true
+end
+
+guess = 37
+puts Benchmark.measure { prime?(guess) }
+puts prime?(guess)
