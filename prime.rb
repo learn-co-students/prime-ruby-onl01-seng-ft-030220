216f72c8736786e4754fc2223cf8eb4a95ed89fd
Charles Butler <cdbutle@outlook.com>
2020-03-13 22:57:58 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  two = 2
+  if num > 1
+    range = (two..num-1).to_a
+    range.none? do |t_test| 
+      num % t_test == 0
+    end
+  else
+    false
+  end
+end
